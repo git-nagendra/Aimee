7af82c9429f388c5e3001ebfbc3b06d77a2d8b7d
dragon-ninja <129362302+dragon-ninja@users.noreply.github.com>
2024-04-16 17:45:04 +0530

    update

--- assest/js/app.js
+++ assest/js/app.js
@@ -4,3 +4,9 @@ ham.addEventListener('click',()=>{
     ham.classList.toggle('transform');
     nav.classList.toggle('show_menu')
 })
+
+window.addEventListener("mousemove", (event) => {
+    const cursor =document.querySelector('.cursor');
+    cursor.style.left = event.pageX + 'px';
+    cursor.style.top = event.pageY + 'px';
+});
